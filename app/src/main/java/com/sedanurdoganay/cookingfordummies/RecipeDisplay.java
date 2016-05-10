70cb1ac1a83f9877cb7dbdfc280fefef905fe807
bengisuk <bengisu.kose@ozu.edu.tr>
2016-05-10 17:38:57 +0300

    gradle'ın aptal sorunu

--- app/src/main/java/com/sedanurdoganay/cookingfordummies/RecipeDisplay.java
+++ app/src/main/java/com/sedanurdoganay/cookingfordummies/RecipeDisplay.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 public class RecipeDisplay extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class RecipeDisplay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_display);
         WebView view = (WebView) findViewById(R.id.webView);
+        view.setWebChromeClient(new WebChromeClient());
         view.getSettings().setJavaScriptEnabled(true);
         view.loadUrl("http://www.fatsecret.com/recipes/baked-lemon-snapper/Default.aspx");
 
