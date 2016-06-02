8db38aec05823e10ce432af41e81f5c216e96112
Seda Nur Doganay <seda.doganay@ozu.edu.tr>
2016-06-02 22:04:32 +0300

    -

--- app/src/main/java/com/sedanurdoganay/cookingfordummies/RecipeItem.java
+++ app/src/main/java/com/sedanurdoganay/cookingfordummies/RecipeItem.java
@@ -40,6 +40,13 @@ public class RecipeItem implements BaseColumns{
     private String mealType;
     private byte[] image;
 
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
 
     public RecipeItem(){
 
